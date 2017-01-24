7ab4e34bbf61a74b9cdf82c437c5e0167a3ec97b
Charles <280114413@qq.com>
2017-01-23 19:51:04 -0800

    add algorithm conversion

--- SeqAlignment/Src/main.cpp
+++ SeqAlignment/Src/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include "CalculateScore.h"
+#include "DeliverConversion.h"
 float W[128][128];
 
 
 int main(int argc, char** argv)
 {
     int command;
-    std::cout << "Score:" << std::endl;
+    
     const int M = 5, N = 3;
     char A[M] = {'a', 'g', 't', 'a', 'c'};
     char B[N] = {'a', 'a', 'g'};
     // initialize W matrix
-    
     for(int i = 0; i < 128; ++i)
     {
         for (int j = 0; j < 128; ++j)
@@ -27,7 +27,10 @@ int main(int argc, char** argv)
         }
     }
 
-    std::cout << Cost(M, N, A, B, W) << std::endl;
+    std::cout << "Score:" << Cost(M, N, A, B, W) << std::endl;
+    DeliverConversion(A, B, M, N, W);
+    std::cout << std::endl;
+    std::cout << "Please press any key to exit." << std::endl;
     std::cin >> command;
     return 0;
 }
